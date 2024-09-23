bd35ae782573426b527ad7d0b2a5be2f7524e1aa
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-09-23 06:26:56 +0000

    separates public and private subnets into their own file

---
+++ k8_karpenter_terraform_setup/subnets.tf
@@ -0,0 +1,22 @@
+resource "aws_subnet" "public_subnets" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = element(var.public_subnet_cidrs, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  tags = {
+    Name = "${var.cluster_name}-public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = element(var.private_subnet_cidrs, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+  tags = {
+    "Name" = "${var.cluster_name}-private-subnet-${count.index + 1}"
+     "kubernetes.io/role/internal-elb" = "1"
+     "kubernetes.io/cluster/demo" = "owned"
+  }
+}
