7487900a4d4a077da970a908c3de41a7659212e9
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-09-23 06:47:04 +0000

    adds internet gateway to vpc

--- k8_karpenter_terraform_setup/vpc.tf
+++ k8_karpenter_terraform_setup/vpc.tf
@@ -5,3 +5,11 @@ resource "aws_vpc" "eks_vpc" {
     Name = "${var.cluster_name}-vpc"
   }
 }
+
+# Internet gateway
+ resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+  tags = {
+    Name = "${var.cluster_name}-igw"
+  }
+}
