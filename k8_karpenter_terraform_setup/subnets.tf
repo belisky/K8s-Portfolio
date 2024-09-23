b9f503c6f91f01a334179fdd69b48c8a544089ca
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-09-23 06:37:48 +0000

    updates annotation to use elb for public subnet

--- k8_karpenter_terraform_setup/subnets.tf
+++ k8_karpenter_terraform_setup/subnets.tf
@@ -6,7 +6,7 @@ resource "aws_subnet" "public_subnets" {
   availability_zone       = element(data.aws_availability_zones.available.names, count.index)
   tags = {
     "Name" = "${var.cluster_name}-public-subnet-${count.index + 1}"
-    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/role/elb" = "1"
     "kubernetes.io/cluster/demo" = "owned"
   
   }
