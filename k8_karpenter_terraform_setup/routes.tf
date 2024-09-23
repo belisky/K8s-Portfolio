21de7d66a0984b1a20a180c2c3fbb7edad5ae40e
Nobel Fiawornu <fiawornunobel@gmail.com>
2024-09-23 10:33:58 +0000

    updates count ref

--- k8_karpenter_terraform_setup/routes.tf
+++ k8_karpenter_terraform_setup/routes.tf
@@ -30,7 +30,7 @@ resource "aws_route_table_association" "public_rt_association" {
 }
 
 resource "aws_route_table_association" "private_rt_association" {
-  count          = length(aws_subnet.public_subnets)
+  count          = length(aws_subnet.private_subnets)
   subnet_id      = element(aws_subnet.private_subnets.*.id, count.index)
-  route_table_id = aws_route_table.public_route_table.id
+  route_table_id = aws_route_table.private_route_table.id
 }
